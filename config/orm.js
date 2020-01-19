1b19788437c355b092546f46af93565c53b9a02a
4stru <4.struong@kscholars.org>
2020-01-19 15:52:34 -0600

    get sql data in console

--- config/orm.js
+++ config/orm.js
@@ -1,3 +1,15 @@
 const connection = require("../config/connection.js");
 
+const orm = {
+    all: function(tableInput, cb) {
+      let queryString = "SELECT * FROM " + tableInput + ";";
+      connection.query(queryString, function(err, result) {
+        if (err) {
+          throw err;
+        }
+        cb(result);
+      });
+    }
+}
+
 module.exports = orm;
--- controllers/burgerController.js
+++ controllers/burgerController.js
@@ -2,7 +2,11 @@ const express = require("express");
 
 const router = express.Router();
 
+const burger = require("../models/burger.js");
 router.get('/', (req, res) => {
+    burger.all(function(burger_data){
+        console.log(burger_data)
+    })
     res.render('index')
 })
 
