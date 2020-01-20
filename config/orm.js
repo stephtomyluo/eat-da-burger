9f1fb61b7c97e1c1a948be72315210908779e90d
4stru <4.struong@kscholars.org>
2020-01-19 18:16:08 -0600

    show non devoured burgers

--- config/orm.js
+++ config/orm.js
@@ -1,15 +1,41 @@
 const connection = require("../config/connection.js");
 
-const orm = {
-    all: function(tableInput, cb) {
-      let queryString = "SELECT * FROM " + tableInput + ";";
-      connection.query(queryString, function(err, result) {
-        if (err) {
-          throw err;
-        }
-        cb(result);
-      });
-    }
+function printQuestionMarks(num) {
+  let arr = [];
+
+  for (var i = 0; i < num; i++) {
+    arr.push("?");
+  }
+
+  return arr.toString();
 }
+
+function objToSql(ob) {
+  let arr = [];
+
+  for (var key in ob) {
+    var value = ob[key];
+    if (Object.hasOwnProperty.call(ob, key)) {
+      if (typeof value === "string" && value.indexOf(" ") >= 0) {
+        value = "'" + value + "'";
+      }
+      arr.push(key + "=" + value);
+    }
+  }
+
+  return arr.toString();
+}
+
+const orm = {
+  all: function(tableInput, cb) {
+    let queryString = "SELECT * FROM " + tableInput + ";";
+    connection.query(queryString, function(err, result) {
+      if (err) {
+        throw err;
+      }
+      cb(result);
+    });
+  }
+};
 
 module.exports = orm;
--- controllers/burgerController.js
+++ controllers/burgerController.js
@@ -3,11 +3,17 @@ const express = require("express");
 const router = express.Router();
 
 const burger = require("../models/burger.js");
+
 router.get('/', (req, res) => {
-    burger.all(function(burger_data){
-        console.log(burger_data)
-    })
-    res.render('index')
+
+ 
+    burger.all(function(data) {
+        var hbsObject = {
+          burgers: data
+        };
+        console.log(hbsObject);
+        res.render("index", hbsObject);
+      });
 })
 
 module.exports = router;
